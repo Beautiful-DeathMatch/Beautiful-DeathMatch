1081e97ab613a4d2668e5dcae6323b9e65f4d1dc
totoonion <totoonion@naver.com>
2024-01-15 02:18:11 +0900

    Dictionary로 Weapon시스템 재구성

--- Client/Assets/Scripts/System/Component/WeaponComponent.cs
+++ Client/Assets/Scripts/System/Component/WeaponComponent.cs
@@ -3,120 +3,155 @@ using System.Collections.Generic;
 using System.Reflection;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem; // For Debug
+using System.Text;
+using Microsoft.Unity.VisualStudio.Editor; // For Debug
 
 public class WeaponComponent : MonoComponent<WeaponSystem>
 {
     // class 무기 Instance: {Instance ID, 소유자 ID, 무기 ID, 최대 장탄수, 현재 장탄수, 장전 안한 남은 탄약수}
-    int _ID = 0;
-    int _ownerID;
-    int _weaponID;
-    int _maxMagazine;
-    int _currentMagazine;
-    int _remainedMagazine;
+    [SerializeField]
+    int ID = 0;
 
-    // 무기 상태 변경 (디버깅용)
-    void Change(int ID, int ownerID, int weaponID, int maxMagazine, int currentMagazine, int remainedMagazine)
-    {
-        _ID = ID;
-        _ownerID = ownerID;
-        _weaponID = weaponID;
-        _maxMagazine = maxMagazine;
-        _currentMagazine = currentMagazine;
-        _remainedMagazine = remainedMagazine;
-    }
+    static string _ownerID = "ownerID";
+    static string _weaponType = "weaponType";
+    static string _maxMagazine = "maxMagazine";
+    static string _currentMagazine = "currentMagazine";
+    static string _remainedMagazine = "remainedMagazine";
 
     // =================== 내부 호출용도 =================== //
 
     // 무기 정보 로드 from json DB
     // DBLoad()
 
-    // 이 컴포넌트를 시스템 리스트에 등록
-    void Register()
-    {
-        System.ListUp(this);
-    }
-
 
     // 유효성 검사
-    int Check(){
-        int index = System.FindListIndexByID(_ID);
-        if (index == -1)
+    int Check()
+    {
+        if (ID == 0)
         {
             Debug.Log("경고! 해당 Component가 List에 등록되어있지 않습니다.");
+            return 0;
+        }
+        else if (!System.IsContainsKey(ID))
+        {
+            Debug.Log("경고! 해당 Component의 ID를 찾을 수 없습니다.");
             return -1;
         }
-        else return index;
+        else return ID;
     }
 
     // =================== 외부 반환 용도 =================== //
 
     // ID 반환
-    public int returnID()
+    public int ReturnID()
     {
-        return _ID;
+        return ID;
     }
 
-    // =================== Data 변경 =================== //
+
+    // =================== System 조회 함수 =================== //
+
+    public int LoadOwnerID()
+    {
+        return System.WeaponData(ID, _ownerID);        
+    }
+    public int LoadWeaponType()
+    {
+        return System.WeaponData(ID, _weaponType);        
+    }
+    public int LoadMaxMagazine()
+    {
+        return System.WeaponData(ID, _maxMagazine);        
+    }
+    public int LoadCurrentMagazine()
+    {
+        return System.WeaponData(ID, _currentMagazine);
+    }
+    public int LoadRemainedMagazine()
+    {
+        return System.WeaponData(ID, _remainedMagazine);        
+    }
+
+    // =================== System 요청 함수 =================== //
 
     // 무기 소유자 변경
-    public void Acquire(int ownerID, bool bySystem = false)
+    public void Acquire(int ownerID)
     {
-        _ownerID = ownerID; 
+        System.TryAcquire(ID, ownerID);
     }
 
-    // 무기 버리기 (기획 확인 필요)
-    // Throw(Instance ID)
-
     // 탄약 소모
-    public void Shot(int shotNum = 1)
+    public void Shot()
     {
-        _currentMagazine -= shotNum;
+        System.TryShot(ID);
     }
 
     // 탄약 재장전
-    public void ReLoad()
+    public void Reload()
     {
-        int emptyMegazine = _maxMagazine - _currentMagazine;
-        if(emptyMegazine <= _remainedMagazine)
-        {
-            _currentMagazine += emptyMegazine;
-            _remainedMagazine -= emptyMegazine;
-        } 
-        else
-        {
-            _currentMagazine += _remainedMagazine;
-            _remainedMagazine -= _remainedMagazine;
-        }
+        System.TryReload(ID);
     }
 
     // 무기 삭제 -> 시스템에 삭제 요청
     public void Delete()
     {
-        System.Delete(this);
+        System.TryDelete(ID);
     }
 
-    // 시스템에 의한 삭제: 현재 컴포넌트가 달려 있는 Object 삭제
-    public void DeleteBySystem()
+    // =================== 기타 함수 =================== //
+
+    // 시스템에 의한 ID 설정
+    public void SetID(int id)
+    {
+        ID = id;
+    }
+
+    // 오브젝트 삭제: 현재 컴포넌트가 달려 있는 Object 삭제
+    public void DeleteObject()
     {
         Destroy(this.gameObject);
     }
 
-    // 시스템에 의한 ID 세팅
-    public void SetID(int ID){
-        _ID = ID;
-    }
+    // =================== Update 함수 (유효하지 않은 오브젝트 삭제 용) =================== //
 
-
-    // =================== Awake =================== //
-
-
-    private void Awake() 
+    void Update()
     {
-        // 자동 등록 후 유효성 검사
-        if (_ID == 0)
-            Register();
-        Check();
+        if(Check() == -1)
+        {
+            DeleteObject();
+        }
+
+        // =================== DEBUG =================== //
+
+        if(Input.GetMouseButtonDown(0)){
+            Shot();
+        }
+        if(Input.GetKeyDown(KeyCode.R)){
+            Reload();
+        }
+        if(Input.GetKeyDown(KeyCode.X)){
+            Delete();
+        }
+
     }
 
+    StringBuilder builder_ = new StringBuilder();
+    GUIStyle style_ = new GUIStyle();
+
+    private void OnGUI() {
+
+        builder_.Clear();
+        builder_.AppendFormat("[{0}", LoadOwnerID());
+        builder_.AppendFormat("{0}] ", LoadWeaponType());
+        builder_.AppendFormat("{0} ", LoadCurrentMagazine());
+        builder_.AppendFormat("/ {0} ", LoadMaxMagazine());
+        builder_.AppendFormat("/ {0} ", LoadRemainedMagazine());
+
+        style_.normal.textColor = new Color(0, 0, 0, 1);
+        GUI.Label(
+            new Rect(Screen.width * 0.9f, Screen.height * 0.8f, Screen.width * 0.98f, Screen.height * 0.88f)
+            , builder_.ToString(), style_);
+    }
 
 }
--- Client/Assets/Scripts/System/WeaponSystem.cs
+++ Client/Assets/Scripts/System/WeaponSystem.cs
@@ -2,89 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data.Common;
 using UnityEngine;
+using UnityEngine.InputSystem; // For Debug
 
 public class WeaponSystem : MonoSystem
 {
 
     // 무기 List
-    List<WeaponComponent> weapons = new List<WeaponComponent>();
+    // List<WeaponComponent> weapons = new List<WeaponComponent>();
+    Dictionary<int, Dictionary<string, int>> weapons = new Dictionary<int, Dictionary<string, int>>();
 
-    // 마지막으로 생성된 무기의 고유 ID
+    string _ownerID = "ownerID";
+    string _weaponType = "weaponType";
+    string _maxMagazine = "maxMagazine";
+    string _currentMagazine = "currentMagazine";
+    string _remainedMagazine = "remainedMagazine";
+
+
+    // 마지막으로 생성된 무기의 고유 ID, 1 부터 시작
     static int weaponIDUnique = 0;
 
-
-    // =================== Data 변경 =================== //
-
-    // 무기 소유자 변경
-    public void Acquire(int ID, int ownerID)
-    {
-        weapons[ID].Acquire(ownerID);
-    }
+    // Weapon이 달릴 오브젝트 프리팹
+    [SerializeField]
+    WeaponComponent weaponPrefeb;
     
-    // 무기 버리기 (기획 확인 필요)
-    // Throw(Instance ID)
 
-    // 탄약 소모
-    public void Shot(int ID, int shotNum = 1)
-    {
-        FindByID(ID).Shot(shotNum);
-    }
-
-    // 탄약 재장전
-    public void ReLoad(int ID)
-    {
-        FindByID(ID).ReLoad();
-    }
-
-    // 무기 삭제 (리스트에서 삭제)
-    public void Delete(int ID) // ID로 삭제
-    {
-        int findedIndex = FindListIndexByID(ID);
-        if (findedIndex >= 0 && findedIndex < weapons.Count)
-        {
-            weapons[findedIndex].DeleteBySystem();
-            weapons.RemoveAt(findedIndex);
-        }
-        else
-            Debug.Log("삭제 실패: ID가 올바르지 않습니다.");
-    }
-        
-    public void Delete(WeaponComponent weapon) // weapon 정보로 삭제
-    {
-        if (weapons.Remove(weapon) == false)
-        {
-            Debug.Log("삭제 실패: 해당 Component가 List에 없습니다.");
-            return;
-        }
-        weapon.DeleteBySystem();
-    }
-
-
-    // =================== 써칭/고유ID 관련/기타 =================== //
-
-    // ID에 해당하는 무기의 리스트 번호 찾기
-    public int FindListIndexByID(int ID)
-    {
-        for (int i = 0; i < weapons.Count; i++)
-        {
-            if (weapons[i].returnID() == ID)
-                return i;
-        }
-        Debug.Log(ID + " ID 에 해당하는 Weapon 없음");
-        return -1;
-    }
-
-    // ID에 해당하는 무기 찾기
-    public WeaponComponent FindByID(int ID)
-    {
-        for (int i = 0; i < weapons.Count; i++)
-        {
-            if (weapons[i].returnID() == ID)
-                return weapons[i];
-        }
-        Debug.Log(ID + " ID 에 해당하는 Weapon 없음");
-        return null;
-    }
+    // =================== 생성/삭제 =================== //
 
     // Component 고유 ID 생성 및 반환
     int CreateID()
@@ -93,16 +35,124 @@ public class WeaponSystem : MonoSystem
         return weaponIDUnique;
     }
 
-    // 무기 컴포넌트에 고유 ID 부여 및 리스트업
-    public void ListUp(WeaponComponent weapon){
-        weapon.SetID(CreateID());
-        weapons.Add(weapon);
+    // weapon 생성
+    public void Create(int ownerID, int weaponType, int maxMagazine, int currentMagazine, int remainedMagazine)
+    {
+        int newID = CreateID();
+        weapons.Add(newID, new Dictionary<string, int>(){
+        {_ownerID, ownerID},
+        {_weaponType, weaponType},
+        {_maxMagazine, maxMagazine},
+        {_currentMagazine, currentMagazine},
+        {_remainedMagazine, remainedMagazine}
+        });
+        // WeaponComponent 생성하여 유저에게 할당하는 코드 필요
+        Instantiate(weaponPrefeb, /*ownerID 유저 Object.*/transform).SetID(newID);
+
+    }
+    public void TryCreate(int ownerID, int weaponType, int maxMagazine, int currentMagazine, int remainedMagazine)
+    {
+        Create(ownerID, weaponType, maxMagazine, currentMagazine, remainedMagazine);
     }
 
-    // Component가 System에 Request를 보낼 때 사용하는 함수
-    public void Request(string content){
-        // server로 전달하는 내용?
-
+    // weapon 제거
+    public void Delete(int ID)
+    {
+        weapons.Remove(ID);
+    }
+    public void TryDelete(int ID)
+    {
+        Delete(ID);
     }
 
+
+    // =================== 외부에서의 Data 확인용 =================== //
+
+    // 존재 여부 확인
+    public bool IsContainsKey(int ID)
+    {
+        return weapons.ContainsKey(ID);
+    }
+
+    // Data 확인
+    public int WeaponData(int ID, string dataName)
+    {
+        if (weapons.ContainsKey(ID))
+            return weapons[ID][dataName];
+        else
+        {
+            Debug.Log("Data 조회 실패!");
+            return -1;
+        }
+    }
+
+    // =================== 기능 =================== //
+
+    // 무기 소유자 변경
+    public void Acquire(int ID, int ownerID)
+    {
+        weapons[ID][_ownerID] = ownerID;
+    }
+    public void TryAcquire(int ID, int ownerID)
+    {
+        Acquire(ID, ownerID);
+    }
+
+    // 탄약 소모
+    public void Shot(int ID)
+    {
+        int currentMagazine = weapons[ID][_currentMagazine];
+        if (currentMagazine > 0)
+            weapons[ID][_currentMagazine] -= 1;
+    }
+    public void TryShot(int ID)
+    {
+        Shot(ID);
+    }
+
+    // 탄약 재장전
+    public void Reload(int ID)
+    {
+        int currentMagazine = weapons[ID][_currentMagazine],
+            maxMagazine = weapons[ID][_maxMagazine],
+            remainedMagazine = weapons[ID][_remainedMagazine];
+        int consumedMegazine = maxMagazine - currentMagazine;
+
+        if (consumedMegazine <= remainedMagazine)
+        {
+            currentMagazine += consumedMegazine;
+            remainedMagazine -= consumedMegazine;
+        }
+        else
+        {
+            currentMagazine += remainedMagazine;
+            remainedMagazine -= remainedMagazine;
+        }
+        weapons[ID][_currentMagazine] = currentMagazine;
+        weapons[ID][_maxMagazine] = maxMagazine;
+        weapons[ID][_remainedMagazine] = remainedMagazine;
+    }
+    public void TryReload(int ID)
+    {
+        Reload(ID);
+    }
+
+
+    // ==============DEBUG============= //
+
+    GUIStyle style_ = new GUIStyle();
+
+    private void OnGUI() {
+        style_.normal.textColor = new Color(0, 0, 0, 1);
+        GUI.Label(
+            new Rect(Screen.width * 0.85f, Screen.height * 0.9f, Screen.width * 0.98f, Screen.height * 0.98f)
+            , "Z:Add X:Delete R:Reload " +weapons.Count.ToString(), style_);
+    }
+
+    private void Update() 
+    {
+        if(Input.GetKeyDown(KeyCode.Z)){
+            TryCreate(1, 1, 5, 5, 15);
+        }
+    }
 }
